8b15ac42fd5ce81b99066e3554ef43a084d5085d
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2026-01-20 22:43:46 +0100

    feat: bind workspace manager, add listeners
    the program now correctly identifies existing workspaces and receives
    workspace handles

--- labline.c
+++ labline.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 #include <wayland-client.h>
 #include "wlr-layer-shell-unstable-v1-client-protocol.h"
 #include "ext-workspace-v1-client-protocol.h"
@@ -14,28 +15,34 @@ static struct wl_registry *registry;
 static struct wl_compositor *compositor;
 static struct wl_shm *shm;
 static struct zwlr_layer_shell_v1 *layer_shell;
-static struct ext_workspace_manager_v1 *workspace_mgr;
+static struct ext_workspace_manager_v1 *workspace_manager;
 
-static void registry_handle_global(void* data, struct wl_registry *wl_registry,
-                                   uint32_t name, const char *iface,
+static int exit_status = 0;
+
+static void registry_handle_global(void* data,
+                                   struct wl_registry *wl_registry,
+                                   uint32_t name,
+                                   const char *iface,
                                    uint32_t version)
 {
      /* i'm okay with some code duplication here */
+     uint32_t v; 
      if (strcmp(iface, wl_compositor_interface.name) == 0) {
-          uint32_t version_to_bind = MIN(version, (uint32_t)wl_compositor_interface.version);
-          compositor = wl_registry_bind(registry, name, &wl_compositor_interface, version_to_bind);
+          v = MIN(version, (uint32_t)wl_compositor_interface.version);
+          compositor = wl_registry_bind(registry, name,
+                                        &wl_compositor_interface, v);
      } else if (strcmp(iface, wl_shm_interface.name) == 0) {
-          uint32_t version_to_bind = MIN(version, (uint32_t) wl_shm_interface.version);
-          shm = wl_registry_bind(registry, name, &wl_shm_interface, version_to_bind);
+          v = MIN(version, (uint32_t) wl_shm_interface.version);
+          shm = wl_registry_bind(registry, name,
+                                 &wl_shm_interface, v);
      } else if (strcmp(iface, zwlr_layer_shell_v1_interface.name) == 0) {
-          uint32_t version_to_bind = MIN(version, (uint32_t) zwlr_layer_shell_v1_interface.version);
-          layer_shell = wl_registry_bind(registry, name, &zwlr_layer_shell_v1_interface,
-                                         version_to_bind);
+          v = MIN(version, (uint32_t) zwlr_layer_shell_v1_interface.version);
+          layer_shell = wl_registry_bind(registry, name,
+                                         &zwlr_layer_shell_v1_interface, v);
      } else if (strcmp(iface, ext_workspace_manager_v1_interface.name) == 0) {
-          uint32_t version_to_bind = MIN(version,
-                                         (uint32_t)ext_workspace_manager_v1_interface.version);
-          workspace_mgr = wl_registry_bind(registry, name, &ext_workspace_manager_v1_interface,
-                                           version_to_bind);
+          v = MIN(version, (uint32_t)ext_workspace_manager_v1_interface.version);
+          workspace_manager = wl_registry_bind(registry, name,
+                                               &ext_workspace_manager_v1_interface, v);
      }
 }
 
@@ -48,22 +55,74 @@ static void registry_handle_global_remove(void *data,
 
 static const struct wl_registry_listener registry_listener = {
      .global = &registry_handle_global,
-     .global_remove = &registry_handle_global_remove,
+     .global_remove = &registry_handle_global_remove
 };
 
-int main() {
-     display = wl_display_connect(NULL);
-     registry = wl_display_get_registry(display);
-     wl_registry_add_listener(registry, &registry_listener, NULL);
-     wl_display_roundtrip(display);
+static void workspace_manager_handle_workspace(void *data,
+                                               struct ext_workspace_manager_v1 *mgr,
+                                               struct ext_workspace_handle_v1 *id)
+{
+     printf("New Workspace handle received: %p\n", (void *)id);
+}
 
+static void workspace_manager_handle_group(void *data,
+                                           struct ext_workspace_manager_v1 *mgr,
+                                           struct ext_workspace_group_handle_v1 *id)
+{
+     printf("New Workspace group received: %p\n", (void *)id);
+}
+
+static void workspace_manager_handle_done()
+{
+}
+
+static void workspace_manager_handle_finished()
+{
+}
+
+static const struct ext_workspace_manager_v1_listener workspace_manager_listener = {
+     .workspace = &workspace_manager_handle_workspace,
+     .workspace_group = &workspace_manager_handle_group,
+     .done = &workspace_manager_handle_done,
+     .finished = &workspace_manager_handle_finished
+};
+
+int main() {     
+     display = wl_display_connect(NULL);
      if (!display) {
-          fprintf(stderr, "Failed to connect to Wayland display\n");
+          fprintf(stderr, "Couldn't connect to the display.");
           return 1;
      }
+     
+     registry = wl_display_get_registry(display);
+     if (!registry) {
+          exit_status = 1;
+          fprintf(stderr, "Couldn't get the registry.");
+          goto disconnect;
+     }
+     wl_registry_add_listener(registry, &registry_listener, NULL);
+
+     wl_display_roundtrip(display);
+     if (!workspace_manager) {
+          exit_status = 1;
+          fprintf(stderr, "Workspace manager not supported by the compositor.");
+          goto destroy_registry;
+     }
+     ext_workspace_manager_v1_add_listener(workspace_manager,
+                                           &workspace_manager_listener,
+                                           NULL);
+     wl_display_roundtrip(display);
+     
 
      while (wl_display_dispatch(display) != -1) {
           /* do stuff */
      }
-     return 0;
+
+destroy_registry:
+     wl_registry_destroy(registry);
+          
+disconnect:
+     wl_display_disconnect(display);
+     
+     return exit_status;
 }
